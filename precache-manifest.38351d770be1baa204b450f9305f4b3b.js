495d7d49c01eae98675a7002cc0c316bff93dbe6
Mesfer <mesfer.vip@gmail.com>
2019-11-12 18:00:55 +0300

    Updates

---
+++ precache-manifest.38351d770be1baa204b450f9305f4b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f59930d8e89cb64cc7f8154794b46569",
+    "url": "/project2/index.html"
+  },
+  {
+    "revision": "bc3b8bebdd75518b4ec4",
+    "url": "/project2/static/css/main.342acf38.chunk.css"
+  },
+  {
+    "revision": "6c6a9d8ed8868d056bd5",
+    "url": "/project2/static/js/2.08c31f48.chunk.js"
+  },
+  {
+    "revision": "bc3b8bebdd75518b4ec4",
+    "url": "/project2/static/js/main.37784bf0.chunk.js"
+  },
+  {
+    "revision": "ad132fdf70c7debb2276",
+    "url": "/project2/static/js/runtime-main.48c04930.js"
+  }
+]);
